14c6dfa5422690ad6474e2b9fbfb4473f5021491
Kennedy Slawinski <kMac@Kennedys-MacBook-Pro.local>
2015-09-29 18:25:35 -0600

    broke ground check ned to fix

--- Assets/Scripts/SimplePlatformController.cs
+++ Assets/Scripts/SimplePlatformController.cs
@@ -10,26 +10,61 @@ public class SimplePlatformController : MonoBehaviour {
 	public float maxSpeed = 5f;
 	public float jumpForce = 1000f;
 	public Transform groundCheck;
+	public Transform spawn;
 
 	private bool grounded = false;
 	private Animator anim;
 	private Rigidbody2D rb2d;
 
 
+
+
 	// Use this for initialization
 	void Awake () {
+		transform.position = spawn.position;
 		anim = GetComponent<Animator> ();
 		rb2d = GetComponent<Rigidbody2D> ();
-
+		rb2d.isKinematic = true;
+		//rb2d.isKinematic = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		rb2d.isKinematic = false; // crappy hack to prevent jumping
 		grounded = Physics2D.Linecast (transform.position, groundCheck.position, 1 << LayerMask.NameToLayer ("Ground"));
 		if (Input.GetButtonDown ("Jump") && grounded) {
 			jump = true;
+		} else if (!grounded) { // animate when falling
+			anim.SetTrigger ("Jump"); 
 		}
+
+		if (transform.position.y < -10) {
+			//respawn
+			rb2d.velocity = Vector2.zero;
+			transform.position = spawn.position;
+		}
+
+		if (rb2d.velocity.y > 0) {
+			Debug.Log (rb2d.velocity.y);
+			Physics2D.IgnoreLayerCollision (LayerMask.NameToLayer ("Player"), 
+			                               LayerMask.NameToLayer ("Ground"),
+			                               true);
+		}
+		if (rb2d.velocity.y < 0) {
+			Debug.Log (rb2d.velocity.y);
+			Physics2D.IgnoreLayerCollision (LayerMask.NameToLayer ("Player"), 
+			                                LayerMask.NameToLayer ("Ground"),
+			                                false);
+		}
+		/* else if (rb2d.velocity.y > 0 && !grounded) {
+			Debug.Log (rb2d.velocity.y);
+			Physics2D.IgnoreLayerCollision (LayerMask.NameToLayer ("Player"), 
+			                               LayerMask.NameToLayer ("Ground"),
+			                               true);
+		}*/
+
 	}
+
 
 	void Flip(){
 		facingRight = !facingRight;
